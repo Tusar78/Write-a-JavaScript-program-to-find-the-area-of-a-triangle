62c246bc4d5f80da4a7ed3b6cc2413abfc21b8d1
Tusar <tusarahmed141@gmail.com>
2020-02-17 19:47:43 +0600

    Write a JavaScript function to find the area of a triangle

---
+++ app.js
@@ -0,0 +1,15 @@
+// var a = 5;
+// var b = 6;
+// var c = 7;
+// var s = (a + b + c) / 2;
+// var area = Math.sqrt(s*((s-a) * (s-b) * (s-c)))
+// console.log(area)
+
+// ES 6 Version
+
+let a = 5;
+let b = 6;
+let c = 7;
+let s = (a + b + c) / 2;
+let area = Math.sqrt(s*((s-a)*(s-b)*(s-c)));
+console.log(area);
